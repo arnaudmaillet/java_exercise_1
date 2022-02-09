1c00981c0b867fe5f1ac24f5535636e0b5aff71b
Arnaud Maillet <arnaud97.nono@gmail.com>
2022-02-09 19:43:38 +0100

    Partie_3

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,8 +4,11 @@ public class Launcher {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Bienvenue !");
-        System.out.println("Entrer une string : ");
-        if (!new String(scan.nextLine()).equals("quit")) System.out.println("Unknown command");
+        System.out.println("Entrer une commande : ");
+        while (!new String(scan.nextLine()).equals("quit")) {
+            System.out.println("Unknown command");
+            System.out.println("Entrer une commande : ");
+        }
     }
 }
 

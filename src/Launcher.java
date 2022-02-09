1e6a8b8718c371d29938b25a58c94b85cb632acb
Arnaud Maillet <arnaud97.nono@gmail.com>
2022-02-09 22:29:55 +0100

    Partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,10 +5,23 @@ public class Launcher {
         Scanner scan = new Scanner(System.in);
         System.out.println("Bienvenue !");
         System.out.println("Entrer une commande : ");
-        while (!new String(scan.nextLine()).equals("quit")) {
-            System.out.println("Unknown command");
+        while (true) {
+            String str = new String(scan.next());
+            if (str.equals("quit")) {
+                break;
+            } else if (str.equals("fibo")) {
+                System.out.println("Entrer un nombre: ");
+                System.out.println("Fibonacci result : " + fibo(scan.nextInt()));
+            } else {
+                System.out.println("Unknown command");
+            }
             System.out.println("Entrer une commande : ");
         }
+    }
+
+    private static int fibo(int n) {
+        if (n <= 1) return n;
+        else return fibo(n-1) + fibo(n-2);
     }
 }
 
